71b1fa5a2b2d86ec7781e39f76376b326367ea68
donghun0508 <alswn4516@gmail.com>
2025-12-19 11:30:00 +0900

    remove unused imports in WebConfiguration for cleaner code

--- module/code-crew-web/src/main/java/site/codecrew/web/autoconfigure/WebConfiguration.java
+++ module/code-crew-web/src/main/java/site/codecrew/web/autoconfigure/WebConfiguration.java
@@ -1,8 +1,6 @@
 package site.codecrew.web.autoconfigure;
 
-import java.util.List;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
