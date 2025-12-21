12be8eb5a90f1a6c2914a53056856be4cc1b139a
donghun0508 <alswn4516@gmail.com>
2025-12-21 10:27:22 +0900

    fix: refactor toClaims method in SocialProfile to use EnumMap and Optional for claims

--- services/codecrew-service-account/src/main/java/site/codecrew/account/domain/SocialProfile.java
+++ services/codecrew-service-account/src/main/java/site/codecrew/account/domain/SocialProfile.java
@@ -1,19 +1,27 @@
 package site.codecrew.account.domain;
 
+import static java.util.Map.entry;
+
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import site.codecrew.account.application.token.ClaimKey;
 import site.codecrew.account.application.token.JsonWebTokenClaims;
 
 public record SocialProfile(SocialType socialType, String sub, String email, String name, String picture) {
 
     public JsonWebTokenClaims toClaims() {
-        return new JsonWebTokenClaims(sub,
-            Map.of(
-                ClaimKey.SOCIAL_TYPE, socialType().name(),
-                ClaimKey.EMAIL, email(),
-                ClaimKey.NAME, name(),
-                ClaimKey.PICTURE, picture())
-        );
+        Map<ClaimKey, Object> claims = new EnumMap<>(ClaimKey.class);
+        claims.put(ClaimKey.SOCIAL_TYPE, socialType().name());
+
+        Optional.ofNullable(email()).ifPresent(v -> claims.put(ClaimKey.EMAIL, v));
+        Optional.ofNullable(name()).ifPresent(v -> claims.put(ClaimKey.NAME, v));
+        Optional.ofNullable(picture()).ifPresent(v -> claims.put(ClaimKey.PICTURE, v));
+
+        return new JsonWebTokenClaims(sub, claims);
     }
 
     public static SocialProfile fromClaims(JsonWebTokenClaims claims) {
