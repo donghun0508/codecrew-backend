1da17ab35df2996edd491ea89834f0b4b9f3da80
donghun0508 <alswn4516@gmail.com>
2026-01-06 21:47:12 +0900

    fix: add Spring Boot Actuator and Micrometer dependencies in build.gradle.kts

--- module/codecrew-logging/build.gradle.kts
+++ module/codecrew-logging/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     id("spring-boot-logging")
 }
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+}
--- module/codecrew-monitoring/build.gradle.kts
+++ module/codecrew-monitoring/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id("spring-boot-monitoring")
 }
+
+dependencies {
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+}
